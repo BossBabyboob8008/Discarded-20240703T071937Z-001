072d847f8379e2d6cfae0f768e8d586ac715e1de
BossBabyboob8008 <carwil11@kingshigh.school.nz>
2024-09-04 22:30:32 +1200

    exit portal added
    yeet

--- death.gd
+++ death.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree(). reload_current_scene()
+	if body.is_in_group("Player") and Input.is_action_pressed("E"):
+		get_tree(). change_scene_to_file("res://level_2.tscn")
 		
